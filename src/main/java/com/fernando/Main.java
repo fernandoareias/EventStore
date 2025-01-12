2c184a21a84f5d2af301724b5751e38a633e8b06
Fernando Areias <nando.calheirosx@gmail.com>
2025-01-12 11:33:12 -0300

    Add event store

--- src/main/java/com/fernando/Main.java
+++ src/main/java/com/fernando/Main.java
@@ -3,28 +3,33 @@ package com.fernando;
 import com.fernando.events.BankAccountOpened;
 import com.fernando.infrastructure.EventStore;
 
+import java.io.IOException;
 import java.time.Instant;
 import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
 
-        var eventStore = new EventStore();
-        eventStore.append(new BankAccountOpened(
-                UUID.randomUUID(),
-                "8257388405513933",
-                UUID.randomUUID(),
-                "USD",
-                Instant.now(),
-                1L
-        ));
+        try(var eventStore = new EventStore())
+        {
+            eventStore.append(new BankAccountOpened(
+                    UUID.randomUUID(),
+                    "8257388405513933",
+                    UUID.randomUUID(),
+                    "USD",
+                    Instant.now(),
+                    1L
+            ));
 
-        System.out.println("Iniciando leitura");
-        eventStore.stream(System.out::println);
+            System.out.println("Iniciando leitura");
+            eventStore.stream(System.out::println);
 
-        System.out.println("Lendo o evento de offset 4");
-        eventStore.consume(System.out::println, 4L);
-
-        System.out.println("Hello world!");
+            System.out.println("Lendo o evento de offset 4");
+            eventStore.consume(System.out::println, 4L);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/fernando/infrastructure/EventStore.java
+++ src/main/java/com/fernando/infrastructure/EventStore.java
@@ -1,28 +1,54 @@
 package com.fernando.infrastructure;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.*;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.StandardOpenOption;
 import java.util.function.Consumer;
 
-public class EventStore {
+public class EventStore implements AutoCloseable {
 
     private static final String path = "eventStore.txt";
+    private MappedByteBuffer buffer;
+    private FileChannel fileChannel;
+    private RandomAccessFile file;
     private ObjectMapper objectMapper = new ObjectMapper();
 
     public EventStore() {
         ensureFileExists();
         objectMapper.registerModule(new JavaTimeModule());
+        mapFileToMemory();
+    }
+
+    private void mapFileToMemory() {
+        try {
+            file = new RandomAccessFile(path, "rw");
+            fileChannel = file.getChannel();
+            long fileLength = file.length();
+            buffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, fileLength);
+        } catch (IOException e) {
+            System.err.println("Erro ao mapear o arquivo para memória: " + e.getMessage());
+        }
     }
 
     public void append(Object event) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path, true))) {
+        try {
             String jsonEvent = convertObjectToJson(event);
+            byte[] eventBytes = jsonEvent.getBytes(StandardCharsets.UTF_8);
 
-            writer.append(jsonEvent);
-            writer.newLine();
+            long currentLength = buffer.limit();
+            long newLength = currentLength + eventBytes.length + 1;
 
+            buffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, newLength);
+
+            buffer.position((int) currentLength);
+            buffer.put(eventBytes);
+            buffer.put((byte) '\n');
         } catch (IOException e) {
             System.err.println("Erro ao tentar escrever no arquivo de event store: " + e.getMessage());
         }
@@ -31,21 +57,19 @@ public class EventStore {
     public void consume(Consumer<String> action, Long offset) {
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
             String line = null;
-            for (int i = 1; i < offset; i++) {
+            for (int i = 1; i <= offset; i++) {
                 line = reader.readLine();
             }
 
-            if (line != null) {
-                action.accept(line);
-            } else {
+            if(line == null){
                 System.out.println("O arquivo não tem tantas linhas.");
+                return;
             }
+            action.accept(line);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-
 
     public void stream(Consumer<String> action) {
         try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
@@ -71,12 +95,25 @@ public class EventStore {
         }
     }
 
-    private String convertObjectToJson(Object object) {
+    private String convertObjectToJson(Object object) throws JsonProcessingException {
         try {
             return objectMapper.writeValueAsString(object);
         } catch (IOException e) {
             System.err.println("Erro ao converter objeto para JSON: " + e.getMessage());
-            return "";
+            throw e;
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (buffer != null) {
+            buffer.clear();
+        }
+        if (fileChannel != null) {
+            fileChannel.close();
+        }
+        if (file != null) {
+            file.close();
         }
     }
 }
